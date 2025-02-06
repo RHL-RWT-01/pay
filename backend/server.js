66400bfdcedf7cd032b0908b4e86b5d82c61bf45
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-06 23:29:15 +0530

    add CORS middleware to backend for handling cross-origin requests

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 app.get('/',(req,res)=>{
     res.json({ message: 'Welcome to the REST API!' });
 })
