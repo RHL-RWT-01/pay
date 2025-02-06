71a876805953d3536b42e8f66e73158a5d065eb3
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-07 00:32:27 +0530

    add amount model for tracking user transactions

---
+++ backend/db/amount.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const amountSchema = mongoose.Schema({
+    amount: { type: Number, required: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true
+    }
+})
+const Amount = mongoose.model("Amount", amountSchema);
+
+export default Amount;
