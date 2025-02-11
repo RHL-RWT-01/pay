d436a6b8ac622ad543d80c447bd807d41033f0c3
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-11 21:16:34 +0530

    add Chakra UI integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {ChakraProvider } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')).render(
+// const theme = extendTheme({});
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
