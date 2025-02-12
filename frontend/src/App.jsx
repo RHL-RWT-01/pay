f1a1c8bad6732bda5702c023ff178de3c2bfeaaf
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-12 23:26:40 +0530

    add authentication check with loading spinner in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
-import { Button, VStack } from "@chakra-ui/react";
+import { Button, Spinner, VStack } from "@chakra-ui/react";
 import Login from "./compos/Login";
 import Signup from "./compos/Signup";
 import Home from "./compos/Home";
+import { useQuery } from "@tanstack/react-query";
 function App() {
+  //TODO => add api url to validate me in BE and here also
+  const {
+    data: isAuthenticated,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: "isAuthenticated",
+    queryFn: async () => {
+      try{
+        const res=await fetch("url");
+        if(!res.ok) throw new Error("Failed to authenticate");
+        return res.json();
+      }catch(e){
+        throw new Error("Failed to authenticate");
+      }
+    }
+  });
+
+  if(isLoading)return <Spinner size='md'></Spinner>
   return (
     <>
       <VStack>
