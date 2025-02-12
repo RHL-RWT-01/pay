64b0ca6ea45aa0d6a684865a1a62864411ec67c1
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-12 22:58:12 +0530

    implement Home component with user search functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Button, VStack } from "@chakra-ui/react";
 import Login from "./compos/Login";
 import Signup from "./compos/Signup";
+import Home from "./compos/Home";
 function App() {
   return (
     <>
       <VStack>
         {/* <Login /> */}
-        <Signup />
+        {/* <Signup /> */}
+        <Home />
       </VStack>
     </>
   );
--- frontend/src/compos/Home.jsx
+++ frontend/src/compos/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Button, HStack, Input } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 function Home() {
+  const [searchUser, setSearchUser] = useState("");
+  const handleSearch = () =>{
+    console.log(searchUser)
+  }
   return (
-    <div>Home</div>
-  )
+    <>
+      <HStack>
+        <Input
+          w="800px"
+          onChange={(e) => setSearchUser(e.target.value)}
+          placeholder="search User"
+        ></Input>
+        <FaSearch onClick={handleSearch} size="25px" />
+      </HStack>
+    </>
+  );
 }
 
-export default Home
+export default Home;
