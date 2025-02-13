b4a353db9fa95244046c59ec55fd8f639f44e562
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-13 21:11:23 +0530

    Users component to adjust user card width for improved layout

--- frontend/src/compos/Users.jsx
+++ frontend/src/compos/Users.jsx
@@ -7,7 +7,7 @@ function Users({ users }) {
       <VStack w='100%' p={4} >
       {dummy.map((user, index) => {
         return (
-          <HStack key={index} w='100%' justifyContent='space-between' p={4} boxShadow='md' borderRadius='lg' shadow='md' 
+          <HStack key={index} w='30%' justifyContent='space-between' p={4} boxShadow='md' borderRadius='lg' shadow='md' 
           borderColor='gray.900' borderWidth='1px'>
             <Heading>{user.name}</Heading>
             <Button size='xs' 
