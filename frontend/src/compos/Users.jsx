943898ef3d88ea8c64cf4343eafe91e38d608d4b
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-16 22:46:54 +0530

    implement transfer functionality

--- frontend/src/compos/Users.jsx
+++ frontend/src/compos/Users.jsx
@@ -1,7 +1,21 @@
 import { Button, Heading, HStack, VStack } from "@chakra-ui/react";
 import React from "react";
+
+import axios from "axios";
+
+
 const dummy=[{name:"Rahul"},{name:"Sanjay"},{name:"Anurag"}]
 function Users({ users }) {
+
+  const handleTransfer = async () => {
+    try{
+      const response = await axios.post(`/api/v1/user/transactions/transfer`,{
+        amount: 100,
+        recipientId: user._id
+      });
+      console.log(response.data);
+    }
+  }
   return (
     <>
       <VStack w='100%' p={4} >
@@ -10,7 +24,11 @@ function Users({ users }) {
           <HStack key={index} w='30%' justifyContent='space-between' p={4} boxShadow='md' borderRadius='lg' shadow='md' 
           borderColor='gray.900' borderWidth='1px'>
             <Heading>{user.name}</Heading>
-            <Button size='xs' 
+            <Button
+            onClick={
+              handleTransfer()
+            }
+            size='xs' 
             _hover={{ bg: "black", color:"white", borderColor:"blue" }}
             >Send</Button>
           </HStack>
