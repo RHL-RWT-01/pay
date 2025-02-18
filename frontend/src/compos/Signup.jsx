24748ba156de01c51626103faf999ac885b408c7
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-18 22:39:48 +0530

    signup component to use react-query for mutation handling and error logging

--- frontend/src/compos/Signup.jsx
+++ frontend/src/compos/Signup.jsx
@@ -1,22 +1,32 @@
 import { Box, VStack, Input, Button } from "@chakra-ui/react";
+import { useMutation } from "@tanstack/react-query";
 import React, { useState } from "react";
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
-  async function handleSignup() {
-    const res = await fetch("/api/v1/user/signup", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, email, password }),
-    });
-    if (!res.ok) {
-      throw new Error("Failed to signup");
-    }
-    const data = await res.json();
-    console.log(data);
-  }
+
+  const {mutate:handleSignup} = useMutation({
+    mutationFn: async () => {
+      const response = await fetch("/api/v1/user/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+      const resData = await response.json();
+      return resData;
+    },
+    retry: 0,
+    onSuccess: (data) => {
+      console.log("Data:", data);
+    },
+    onError: (error) => {
+      console.log("Error:", error);
+    },
+  })
   return (
     <VStack spacing={4} w="300px" p={4} boxShadow="md" borderRadius="lg">
       <Box bg="teal.900" p={4} rounded="lg">
