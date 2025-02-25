2856125191b306d9d78e664ed3498e70ae17636e
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-25 20:51:45 +0530

    style: update border color and width in Users component

--- frontend/src/compos/Users.jsx
+++ frontend/src/compos/Users.jsx
@@ -30,7 +30,7 @@ function Users({ users }) {
       {dummy.map((user, index) => {
         return (
           <HStack key={index} w='30%' justifyContent='space-between' p={4} boxShadow='md' borderRadius='lg' shadow='md' 
-          borderColor='gray.900' borderWidth='1px'>
+          borderColor='gray.700' borderWidth='2px'>
             <Heading>{user.name}</Heading>
             <Button
             onClick={
