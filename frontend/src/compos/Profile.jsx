9ce4ec832a1efda46f3fa53586f762196da47135
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-27 22:05:54 +0530

    fix spinner

--- frontend/src/compos/Profile.jsx
+++ frontend/src/compos/Profile.jsx
@@ -3,7 +3,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import { BsBox } from "react-icons/bs";
 
 const Profile = () => {
-  const { data:balance, isLoading } = useQueryClient({
+  const { data: balance, isLoading } = useQueryClient({
     queryKey: ["balance"],
     queryFn: async () => {
       const response = await fetch("/api/v1/transactions/balance", {
@@ -20,10 +20,10 @@ const Profile = () => {
   });
   return (
     <>
-      <Button colorScheme="red" size="lg" variant="outline"
-       >{isLoading?<Spinner/>:"Balance"}
+      <Button colorScheme="red" size="lg" variant="outline">
+        {isLoading ? <Spinner size='md' /> : "Balance"}
       </Button>
-     {balance && <h1>{balance}</h1>}
+      {balance && <h1>{balance}</h1>}
     </>
   );
 };
